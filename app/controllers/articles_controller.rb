61536e2294ef29c33856daef26381f252ce15f0d
Erik <erikbolt11@gmail.com>
2023-09-06 02:08:56 -0700

    Added UIkit css and messed with routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
+  mount Motor::Admin => '/admin'
 
+  get "/articles", to: "articles#index" 
 
-  get "/articles", to: "articles#index"
+  get "/home", to: "articles#home"
 
+  get "/home/secondhome", to: "articles#home", as: "fakehome"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
